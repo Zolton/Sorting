11707b11fa3d14ee416e783e601f0e8ec964adb4
Zolton <gmzesq@gmail.com>
2019-11-05 17:11:32 -0500

    MVP done

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,17 +1,17 @@
 # TO-DO: Complete the selection_sort() function below 
-testArray = [64, 25, 12, 22, 11]
+testArray = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
 
-def selection_sort( array ):
+def selection_sort(array):
     # loop through n-1 elements
     
     for i in range(len(array)):
         # Create a variable to be used later, set it equal to whatever value I'm currently
         min_index = i
-        print("min index inside first loop: ", min_index)
+        #print("min index inside first loop: ", min_index)
         for j in range (i+1, len(array)):
             if array[j] < array[min_index]:
                 min_index = j
-                print("min index inside second loop: ", min_index)
+                #print("min index inside second loop: ", min_index)
         array[i], array[min_index] = array[min_index], array[i]         
     return array
 
@@ -24,16 +24,17 @@ print("selection sort: ", selection_sort(testArray))
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
 
-
+testArray2 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( array ):
-    for i in range(len(array)):
-        for j in range (i+1, len(array)):
-            if array[j-1] > array[j] : 
-                array[j-1], array[j] = array[j], array[j-1]
+    for i in range(0,len(array)):
+        #j = i + 1
+        for j in range (0, len(array)):
+            if array[j] > array[i] : 
+                array[j], array[i] = array[i], array[j]
     return array
 
-print("Bubble Sort: ", bubble_sort(testArray))
+print("Bubble Sort: ", bubble_sort(testArray2))
 
 
 # STRETCH: implement the Count Sort function below
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,66 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
-    # TO-DO
+# TO-DO: complete the helper function below to merge 2 sorted arrays
+
+# arr1 = [1,3,5,7]
+# arr2 = [2,4,6,8]
+# def merge( arrA, arrB, merged_arr = [] ):
+#     #elements = len( arrA ) + len( arrB )
+#     #merged_arr = [0] * elements
+#     # TO-DO
     
-    return merged_arr
+#     if len(arrA) == 0 or len(arrB) == 0:
+#         return merged_arr
+#     elif arrA[0] < arrB[0]:
+#         print("arrA", arrA[0])
+#         merged_arr.append(arrA[0])
+#         arrA.pop(0)
+#         merge(arrA, arrB)
+#     elif arrB[0] < arrA[0]:
+#         print("arrB", arrB[0])
+#         merged_arr.append(arrB[0])
+#         arrB.pop(0)
+#         merge(arrA, arrB)
+
+# print("merge solution", merge(arr1, arr2))
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
-    # TO-DO
+# def merge_sort( arr ):
+#     # TO-DO
 
-    return arr
+#     return arr
+
+arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+def merge(left, right):
+    """Merge sort merging function."""
+
+    left_index, right_index = 0, 0
+    result = []
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+
+    result += left[left_index:]
+    result += right[right_index:]
+    return result
+
+
+def merge_sort(array):
+    """Merge sort algorithm implementation."""
+
+    if len(array) <= 1:  # base case
+        return array
+
+    # divide array in half and merge sort recursively
+    half = len(array) // 2
+    left = merge_sort(array[:half])
+    right = merge_sort(array[half:])
+
+    return merge(left, right)
+print("fin", merge_sort(arr1))
 
 
 # STRETCH: implement an in-place merge sort algorithm
